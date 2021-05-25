e276544e7d4926f555d2be2105eab577cc534d92
Humza Sajid <pak786hamzasajid66@Gmail.com>
2021-05-25 22:47:23 +0500

    JS Button Function
    3 different functions are created to practice js

---
+++ Topic 1: Adding Scripts/ExternalJsButton/app.js
@@ -0,0 +1,29 @@
+//The 'red' function will get execute on clicking the red button;
+
+var red=document.getElementById('red');
+
+function Red() {
+
+window.alert("This is the alert for "+red.value+"  button’." );
+
+}
+
+//The 'Green' function will get execute on clicking the red button;
+
+var Green=document.getElementById('Green');
+
+function GreenFun() {
+
+window.alert("This is the alert for "+Green.value+"  button’." );
+
+}
+
+//The 'Blue' function will get execute on clicking the red button;
+
+var Blue=document.getElementById('Blue');
+
+function BlueFun() {
+
+window.alert("This is the alert for "+Blue.value+"  button’." );
+
+}
