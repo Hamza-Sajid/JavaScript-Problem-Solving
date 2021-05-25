e3324689d55c41ea5c5dda118bf1dcfbd20df18c
Humza Sajid <pak786hamzasajid66@Gmail.com>
2021-05-25 22:55:18 +0500

    Variables in JS
    Different types of variables in JS

---
+++ 2. Topic: JS Variables/DefineVariables/app.js
@@ -0,0 +1,26 @@
+//variable in js
+
+//string
+var carName1 = "Pakistan AFIP";   // Using double quotes
+var carName2 = 'HITEC';   // Using single quotes
+
+console.log(carName1);
+
+//NUMBERS
+
+var x1 = 34.00;     // Written with decimals
+var x2 = 34;        // Written without decimals
+
+console.log(x1);
+//BOOLb
+
+var x=true;
+console.log(x);
+//ARRAYS
+var cars = ["Saab", "Volvo", "BMW"];
+
+//OBJETCS
+
+var person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+
+console.log(person.firstName);
