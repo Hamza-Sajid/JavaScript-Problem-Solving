e54773f3e193ef90c02cc8851101f79e1eeacf63
Humza Sajid <pak786hamzasajid66@Gmail.com>
2021-05-25 23:00:02 +0500

    Constant
    Constant concept in js

---
+++ 2. Topic: JS Variables/DefineConstants/app.js
@@ -0,0 +1,16 @@
+//Numeric constant
+
+const  num1=3.169;
+console.log(num1);
+
+
+//STRING constant
+const string="Hamza";
+console.log(string);
+
+
+//BOOL CONSTANTS
+
+const war=false;
+
+console.log(war);
