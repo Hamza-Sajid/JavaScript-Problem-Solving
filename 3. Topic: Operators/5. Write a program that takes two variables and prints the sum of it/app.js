165897a70c8e7284b9eff45027308ddeff295420
Humza Sajid <pak786hamzasajid66@Gmail.com>
2021-05-27 15:45:57 +0500

    Program to add 2 numbers
    adding in JS

---
+++ 3. Topic: Operators/5. Write a program that takes two variables and prints the sum of it/app.js
@@ -0,0 +1,9 @@
+// store input numbers
+const num1 = parseInt(prompt('Enter the first number '));
+const num2 = parseInt(prompt('Enter the second number '));
+
+//add two numbers
+const sum = num1 + num2;
+
+// display the sum
+document.write("<h1>The sum is:  </h1> "+sum);
