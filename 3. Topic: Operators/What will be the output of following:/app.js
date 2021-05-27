0c2427047729430e8f6bc7896522df02730ef280
Humza Sajid <pak786hamzasajid66@Gmail.com>
2021-05-27 15:02:51 +0500

    Console for operator
    console.log(opeator);

---
+++ 3. Topic: Operators/What will be the output of following:/app.js
@@ -0,0 +1,16 @@
+var x = 5, y = 10, z = 15;
+
+console.log(x + y);
+
+
+console.log(y - x);
+
+console.log(x * y);
+
+console.log(y / x);
+
+console.log(x % 2);
+
+console.log(x++);
+
+console.log(x--);
