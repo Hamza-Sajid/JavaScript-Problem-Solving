0a679fc0b564811394194d78a7776ea32f271c07
Humza Sajid <pak786hamzasajid66@Gmail.com>
2021-06-02 16:53:56 +0500

    Add files via upload

---
+++ JS Coding TASKS 3/1 & 2 Tasks Solution/app.js
@@ -0,0 +1,34 @@
+//Task 1- Comparision Operator
+/*
+There are 8 Comparision Operator In Js
+
+== equal to
+
+=== equal value & equal type
+
+!= not equal // TODO:
+
+!== not equal/ not equaltype
+
+> greater then
+
+< less then
+
+>= greater then equall
+
+<= less then equall
+
+*/
+
+ console.log(4<=5);
+
+
+//Task 2
+
+
+x=3;
+console.log(x);
+x+1;
+console.log(x);
+ x+=3;
+ console.log(x);
