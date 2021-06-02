d833ed8f9a423a78fa5aa94b96f6a91058bbd467
Humza Sajid <pak786hamzasajid66@Gmail.com>
2021-06-02 15:52:42 +0500

    Add files via upload

---
+++ JS Coding TASKS 2/Topic:  Variables/02  Problem Name: variableScope/app.js
@@ -0,0 +1,10 @@
+
+
+var myString=2.3;
+var var1=parseInt(myString);
+var var2=parseFloat(myString);
+
+console.log(var1);
+console.log(typeof(var1));
+console.log(var2);
+console.log(typeof(var2));
