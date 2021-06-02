c0c3009da89e6b6050217816363c323bb0678ee4
Humza Sajid <pak786hamzasajid66@Gmail.com>
2021-06-02 15:41:04 +0500

    Add files via upload

---
+++ JS Coding TASKS 2/Topic:  Variables/01 Problem Name: variableName/app.js
@@ -0,0 +1,15 @@
+var globalVar="I am Global";
+
+
+function post_Value() {
+
+let name="Ali";
+var name2="Ali";
+name2="Ahmad";
+globalVar;
+}
+function get_Value() {
+let name="Ali";
+var name2="Ali";
+name2="Ahmad";
+}
