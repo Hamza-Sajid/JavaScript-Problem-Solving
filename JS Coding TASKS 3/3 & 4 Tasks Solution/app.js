b51bb2d8ce56ecfec81298f3f92000bbabc0fe65
Humza Sajid <pak786hamzasajid66@Gmail.com>
2021-06-02 17:12:32 +0500

    Add files via upload

---
+++ JS Coding TASKS 3/3 & 4 Tasks Solution/app.js
@@ -0,0 +1,25 @@
+console.log("---Problem Number 3----");
+if(5>4 && 2<5)
+{
+console.log("5 is grater then 4 and 2 is less then 5")
+}
+if(2==9 || 2==2)
+{
+  console.log(" 2===2");
+}
+
+
+console.log("---Problem Number 4----");
+//Problem Number 4
+
+var isSnow=false;
+var isRain=false;
+var isSpeed=22;
+
+if(isSnow==false && isRain==false && isSpeed<20)
+{
+  console.log("Rocket is ready to launch");
+}
+else {
+  console.log("Rocket Mission Abborted!");
+}
