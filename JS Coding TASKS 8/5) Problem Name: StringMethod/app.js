37a71a8e1d0479e36745fc63f19738d81a607df4
Humza Sajid <pak786hamzasajid66@Gmail.com>
2021-06-18 18:45:34 +0500

    Add files via upload

---
+++ JS Coding TASKS 8/5) Problem Name: StringMethod/app.js
@@ -0,0 +1,13 @@
+var cityName="Tokyo";
+
+
+//Length function
+console.log(cityName.length);
+
+//IndexOf Function
+console.log(cityName.indexOf('o'));
+
+//To search from variable or paragraph
+const search =  /[o]/;
+;
+console.log(cityName.search(search));
