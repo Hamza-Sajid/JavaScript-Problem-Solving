d3f39b09022ee419e5ad00f8d89f7bdf9ce5b497
Humza Sajid <pak786hamzasajid66@Gmail.com>
2021-06-18 18:30:54 +0500

    Add files via upload

---
+++ JS Coding TASKS 8/2) Problem Name: AccessingObjects/app.js
@@ -0,0 +1,13 @@
+var cities=
+{
+  first : "Krachi",
+  second: "Lahore",
+  third : " Islamabad",
+  fourth : "Quetta"
+};
+
+//first -value
+console.log(cities.first);
+
+//fourth value
+console.log(cities.fourth);
