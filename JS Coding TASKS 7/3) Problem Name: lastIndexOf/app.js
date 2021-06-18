c74ae1c7263b27aa8006c85150ad7dd3c014ce5c
Humza Sajid <pak786hamzasajid66@Gmail.com>
2021-06-18 14:33:27 +0500

    Add files via upload

---
+++ JS Coding TASKS 7/3) Problem Name: lastIndexOf/app.js
@@ -0,0 +1,5 @@
+var arr=["Cricket","Hockey","Badmintan","Swiming","Tenis"];
+
+const searchTerm = 'Cricket';
+
+console.log(`The index of the first "${searchTerm}" from the end is ${arr.lastIndexOf(searchTerm)}`);
