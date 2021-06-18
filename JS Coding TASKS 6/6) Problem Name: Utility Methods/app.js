7245f13109847e9b9d35481de21a063455c81e70
Humza Sajid <pak786hamzasajid66@Gmail.com>
2021-06-18 14:20:29 +0500

    Add files via upload

---
+++ JS Coding TASKS 6/6) Problem Name: Utility Methods/app.js
@@ -0,0 +1,9 @@
+var arr=["Cricket","Hockey","Badmintan","Swiming","Tenis"];
+
+
+arr.unshift("Racing");
+arr.push('Cycling');
+console.log(arr.length);
+//now the length is 7
+console.log(arr[0]);
+console.log(arr[6]);
