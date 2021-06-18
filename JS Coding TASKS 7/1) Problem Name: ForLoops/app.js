fd890346a9b7b9395a78b37b2a98b0de87032255
Humza Sajid <pak786hamzasajid66@Gmail.com>
2021-06-18 14:27:21 +0500

    Add files via upload

---
+++ JS Coding TASKS 7/1) Problem Name: ForLoops/app.js
@@ -0,0 +1,13 @@
+var arr=["Cricket","Hockey","Badmintan","Swiming","Tenis"];
+
+for(var i=0; i<arr.length; i++)
+{
+  if("Cricket"===arr[i])
+  {
+    console.log("Cricket Founded");
+    break;
+  }
+  else {
+    console.log("Not Found");
+  }
+}
