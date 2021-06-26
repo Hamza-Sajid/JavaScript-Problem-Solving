170ab97c2de98fb80e3b184ccf4aabd07c583ca1
Humza Sajid <pak786hamzasajid66@Gmail.com>
2021-06-26 10:18:58 +0500

    Add files via upload

---
+++ C23 - CODING TASKS/app.js
@@ -0,0 +1,11 @@
+var arr=["a","i","o"];
+
+arr.toString();
+
+console.log(arr);
+
+arr.splice(1);
+
+var arr2= ['Using','splice','method','for','the','first','time'];
+arr2.splice(5,1,'second');
+console.log(arr2);
