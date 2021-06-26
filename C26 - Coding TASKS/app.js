3b43b9ce11787a258d1d7d36b8c9ac2746b4bba1
Humza Sajid <pak786hamzasajid66@Gmail.com>
2021-06-26 10:29:49 +0500

    Add files via upload

---
+++ C26 - Coding TASKS/app.js
@@ -0,0 +1,31 @@
+let students = [
+    {
+        name: "Amna",
+        hobbies: ["eating", "cooking"]
+    },
+    {
+        name: "Daniyal",
+        hobbies: ["arts", "shopping"]
+    },
+    {
+        name: "Fahad",
+        hobbies: ["coding", "cooking"]
+    },
+    {
+        name: "Hajra",
+        hobbies: ["sleep", "reading"]
+    }
+];
+//First User
+console.log("Hobbies Of "+students[0].name);
+console.log("1 "+students[0].hobbies[0] );
+console.log("2 "+students[0].hobbies[1] );
+//2nd User
+console.log("Hobbies Of "+students[1].name);
+console.log("1 "+students[1].hobbies[0] );
+console.log("2 "+students[1].hobbies[1] );
+
+//3rd user
+console.log("Hobbies Of "+students[2].name);
+console.log("1 "+students[2].hobbies[0] );
+console.log("2 "+students[2].hobbies[1] );
