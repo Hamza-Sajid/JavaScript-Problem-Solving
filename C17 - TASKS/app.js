1a10117a94acee5ce4d7c92f2bc2860f5698ad2b
Humza Sajid <pak786hamzasajid66@Gmail.com>
2021-06-26 10:11:33 +0500

    Add files via upload

---
+++ C17 - TASKS/app.js
@@ -0,0 +1,32 @@
+//tasl 1
+
+/*var a=10;
+console.log(a);
+*/
+
+//task 2
+var my_num=12;
+
+console.log(typeof(my_num));
+
+my_num=12.32;
+
+console.log(typeof(my_num));
+
+//task 3
+
+var my_bool=true;
+console.log(typeof(my_bool));
+my_bool=2;
+console.log(typeof(my_bool));
+//task 4
+var  my_str="asd";
+console.log(typeof(my_str));
+//now without qutation mark it gives an error
+//my_str=asd;
+
+//task 5
+
+var aw=null;
+
+console.log(typeof(aw));
