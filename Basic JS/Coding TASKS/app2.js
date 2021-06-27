1072ba51e33929c4470169918b76cb0c98ad4b02
Humza Sajid <pak786hamzasajid66@Gmail.com>
2021-06-27 20:28:36 +0500

    Add files via upload

---
+++ Basic JS/Coding TASKS/app2.js
@@ -0,0 +1,48 @@
+//TASK 1
+
+//var &*=asd;
+//yes it's shows error
+
+
+//var sad231;
+//now it didn't show any error
+
+//TASK -2
+
+function asd() {
+  let a=12;
+  var b=12;
+}
+
+function dsa() {
+console.log(a);
+console.log(b);
+}
+dsa();
+
+//it's shows error in both cases
+
+
+//TASK -3
+var my_num=1232;
+
+toString(my_num);
+
+var my_string="2.3";
+
+var as=parseInt(my_string);
+
+var bs=parseFloat(my_string);
+
+console.log(as);
+console.log(bs);
+
+
+//TASK-4
+
+function myFun() {
+var name;
+console.log(name);
+
+//it display undefined
+}
